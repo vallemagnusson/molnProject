a4907c67fcb23a56199723137b8c59d911fda6d5
vallemagnusson <valle.magnusson@gmail.com>
2015-10-21 23:14:57 +0200

    new

---
+++ flaskApp.py
@@ -0,0 +1,95 @@
+#!flask/bin/python
+
+from flask import Flask, jsonify, request, render_template, url_for
+from celery import group
+import sys
+import os
+import time
+from proj import convertFile
+import subprocess
+from plot_result import plot_file
+from save_to_db import to_db, in_db
+
+app = Flask(__name__, template_folder="/home/ubuntu/naca_airfoil")
+
+@app.route('/')
+def form():
+	return render_template('site/form_submit.html')
+
+@app.route('/runsh/', methods=['POST'])
+def runsh():
+	angle_start=request.form['angle_start']
+	angle_stop=request.form['angle_stop']
+	n_angles=request.form['n_angles']
+	n_nodes=request.form['n_nodes']
+	n_levels=request.form['n_levels']
+	num_samples=request.form['num_samples']
+	visc=request.form['visc']
+	speed=request.form['speed']
+	T=request.form['T']
+	print 1, "- - - - - - - - Run start - - - - - - - -"
+	###########################
+	##### Check if exists #####
+	###########################
+	#anglediff=$((($angle_stop-$angle_start)/$n_angles))
+
+	anglediff = (int(angle_stop) - int(angle_start)) / int(n_angles)
+	
+	angles = []
+	for i in range(0, int(n_angles)):
+		print 1, i
+		angle = 0
+		angle = (int(angle_start) + anglediff * i)
+		if in_db("r" + n_levels + "a" + str(angle) + "n" + n_nodes + "N" + num_samples + "v" + visc + "s" + speed + "T" + T + ".msh") == False :
+			print "Ja en vinkel!"
+			angles.append(angle)
+	
+	if len(angles) != 0:
+		print "Nu skickas allt ivag :)"
+		response = group(convertFile.s(angle, n_nodes, n_levels, num_samples, visc, speed, T) for angle in angles)
+		result = response.apply_async()
+		result.get()
+
+
+		########################
+		##### Create *.msh #####
+		########################
+		#time_1 = time.time()
+		#subprocess.call(["./run.sh", angle_start, angle_stop, n_angles, n_nodes, n_levels])
+		#time_2 = time.time()
+		#print 2, time_2 - time_1
+		#############################################
+		##### Convert *.msh to *.xml + lite mer #####
+		#############################################
+		#appLocation = app.root_path
+		#fileLocation = appLocation + "/msh/"
+		#content = sorted(os.listdir(fileLocation))
+		#response = group(convertFile.s(fileName, open(fileLocation+fileName, "r").read()) for fileName in content)
+		#result = response.apply_async()
+		#result.get()
+		#time_3 = time.time()
+		#print 3, time_3 - time_2
+
+
+		####################################
+		#for t in result.get():
+		#	(fileNamePlot, data) = t
+		#	plot_file(fileNamePlot, data)
+		#	to_db(fileNamePlot, "")
+		#os.system("rm -rf  msh/*")
+		#os.system("rm -rf  geo/*")
+		#os.system("mv *.png pictures/")
+		#####################################
+
+		
+		##subprocess.call(["mv", "*.png", "pictures/"])
+
+	return render_template('site/runsh.html', 
+							angle_start=angle_start, 
+							angle_stop=angle_stop, 
+							n_angles=n_angles, 
+							n_nodes=n_nodes, 
+							n_levels=n_levels)
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", debug=True )
---
+++ proj.py
@@ -0,0 +1,113 @@
+#!flask/bin/python
+
+import os
+import json
+import time
+import sys
+import time
+import shutil
+from celery import Celery
+from collections import Counter
+import urllib2
+import subprocess
+
+app = Celery('proj', backend='amqp', broker='amqp://mava:orkarinte@130.238.29.120:5672/app2')
+
+@app.task
+def convertFile(angle, n_nodes, n_levels, num_samples, visc, speed, T):
+	fileName = "r" + n_levels + "a" + str(angle) + "n" + n_nodes + ".msh"
+	fileNameWithoutExtension = os.path.splitext(fileName)[0]
+	subprocess.call(["mkdir", fileNameWithoutExtension])
+	print "Started to process file: " + str(fileName)
+
+	runCheck = subprocess.check_call(["sudo", "./run.sh", str(angle), str(angle), "1", n_nodes, n_levels], cwd=fileNameWithoutExtension + "/")
+	#while runCheck != 0:
+	#	print "run.sh not OK!!!!!"
+	#	runCheck = subprocess.check_call(["./run.sh", str(angle), str(angle), "1", n_nodes, n_levels])
+
+	#appLocation = app.root_path
+	#for i in content:
+	#	if i == "r" + n_levels + "a" + angle + "n" + n_nodes + ".msh":
+	#		fileName = content[i]
+	##########################################
+	##### Conver file from *msh to *.xml #####
+	##########################################
+	#newFile = open(fileName, "w")
+	#newFile.write(mshFile)
+	#newFile = open(fileName, "r")
+	#newFile.close()
+	#!!!!!!!!!!!!!fileLocation = "/home/ubuntu/naca_airfoil/msh/"
+	#!!!!!!!!!!!!!content = sorted(os.listdir(fileLocation))
+	#while fileName not in content:
+	#	print "run.sh not ready"
+	#	content = sorted(os.listdir(fileLocation))
+
+	
+	#!!!!!!!!!!!!!xmlFileName = fileNameWithoutExtension + ".xml"
+	#!!!!!!!!!!!!!print fileNameWithoutExtension
+	#!!!!!!!!!!!!!subprocess.call(["dolfin-convert", "msh/"+fileName, xmlFileName])
+
+	#!!!!!!!!!!!!!fileLocation = "/home/ubuntu/naca_airfoil/"
+	#!!!!!!!!!!!!!content = sorted(os.listdir(fileLocation))
+	#while xmlFileName not in content:
+	#	print "dolfin-convert not ready"
+	#	content = sorted(os.listdir(fileLocation))
+	##########################################
+	########## Copy airfoil to dir ###########
+	##########################################
+	
+	#!!!!!!!!!!!!!subprocess.call(["cp", "-a", "airfoil", fileNameWithoutExtension])
+
+	#!!!!!!!!!!!!!fileLocation = "/home/ubuntu/naca_airfoil/" + fileNameWithoutExtension
+	#!!!!!!!!!!!!!content = sorted(os.listdir(fileLocation))
+	#while "airfoil" not in content:
+	#	print "moving airfoil not ready"
+	#	content = sorted(os.listdir(fileLocation))
+	##########################################
+	########## Run airfoil on file ###########
+	##########################################
+	#!!!!!!!!!!!!!num = str(num_samples)
+	#!!!!!!!!!!!!!visc_s = str(visc)
+	#!!!!!!!!!!!!!speed_s = str(speed)
+	#!!!!!!!!!!!!!T_s = str(T)
+	#!!!!!!!!!!!!!subprocess.call(["./airfoil", num, visc_s, speed_s, T_s, "../" + xmlFileName], cwd=fileNameWithoutExtension+"/")
+	##########################################
+	######### Get drag_ligt.m values #########
+	##########################################
+	#while "results" not in content:
+	#	print "result form airfoil not ready"
+	#	content = sorted(os.listdir(fileLocation))
+	#!!!!!!!!!!!!!resultLists = readFile("/home/ubuntu/naca_airfoil/" +fileNameWithoutExtension+"/results/drag_ligt.m")
+	#!!!!!!!!!!!!!os.system("rm -rf " + fileNameWithoutExtension + "*")
+	#!!!!!!!!!!!!!os.system("rm -rf  msh/*")
+	#!!!!!!!!!!!!!os.system("rm -rf  geo/*")
+	#!!!!!!!!!!!!!return (fileNameWithoutExtension+"N"+num+"v"+visc_s+"s"+speed_s+"T"+T_s+".msh", resultLists)
+	return ("hej",[[],[],[]])
+	
+@app.task
+def readFile(fileName):
+	theFile = open(fileName, "r").read()
+	timeColumn = []
+	liftColumn = []
+	dragColumn = []
+	lines = open(fileName, "r").readlines()
+	for x in range(1, len(lines)):
+		time = lines[x].strip().split()[0]
+		timeColumn.append(time)
+		lift = lines[x].strip().split()[1]
+		liftColumn.append(lift)
+		drag = lines[x].strip().split()[2]
+		dragColumn.append(drag)
+	resultList = []
+	resultList.append(timeColumn)
+	resultList.append(liftColumn)
+	resultList.append(dragColumn)
+	return resultList
+
+
+
+
+
+
+
+
