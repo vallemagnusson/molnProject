6f15deeb92f30232fa72b81f8e125421d0e457e9
vallemagnusson <valle.magnusson@gmail.com>
2015-10-22 09:44:33 +0200

    oki

--- celeryconfig.py
+++ celeryconfig.py
@@ -2,7 +2,7 @@
 BROKER_URL = 'amqp://mava:orkarint@130.238.29.120:5672/app2'
 
 # List of modules to import when celery starts.
-CELERY_IMPORTS = ('app2.proj')
+CELERY_IMPORTS = ('proj', )
 
 ## Using the database to store task state and results.
 CELERY_RESULT_BACKEND = 'amqp://'
