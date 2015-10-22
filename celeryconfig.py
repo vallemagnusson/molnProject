c5875d75c98996301530132bf7a4f2f9dc6250d4
vallemagnusson <valle.magnusson@gmail.com>
2015-10-22 09:32:00 +0200

    update config

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,8 +1,8 @@
 ## Broker settings.
-BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+BROKER_URL = 'amqp://mava:orkarint@130.238.29.120:5672/app2'
 
 # List of modules to import when celery starts.
-CELERY_IMPORTS = ('myapp.tasks', )
+CELERY_IMPORTS = ('app2.proj')
 
 ## Using the database to store task state and results.
 CELERY_RESULT_BACKEND = 'amqp://'
