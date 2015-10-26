ec2a2bf01ee70dcac8350370901ae620c715bb25
vallemagnusson <valle.magnusson@gmail.com>
2015-10-26 16:24:50 +0100

    fail

--- getFloatingIP.py
+++ getFloatingIP.py
@@ -5,7 +5,7 @@ myip = urllib2.urlopen("http://myip.dnsdynamic.org/").read()
 
 print myip
 with open("/etc/profile.d/myvar.sh", "a") as outfile:
-        outfile.write("export FLOATING_IP="+myip"\n")
+        outfile.write("export FLOATING_IP="+myip+"\n")
 
 
 
