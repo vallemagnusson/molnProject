a7eea16058fbb7f946a47b89a059a2892559fedb
vallemagnusson <valle.magnusson@gmail.com>
2015-10-26 16:14:10 +0100

    va fan!

--- getFloatingIP.py
+++ getFloatingIP.py
@@ -4,7 +4,7 @@ import urllib2
 myip = urllib2.urlopen("http://myip.dnsdynamic.org/").read()
 
 print myip
-with open("/etc/environment", "a") as outfile:
+with open("/etc/profile.d/myvar.sh", "a") as outfile:
         outfile.write("export FLOATING_IP="+myip"\n")
 
 
