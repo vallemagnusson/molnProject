87254ce4898c8f0e2b5ab9b2c8f6dc52a1179dc0
vallemagnusson <valle.magnusson@gmail.com>
2015-10-27 00:41:55 +0100

    japp

--- celery.py
+++ celery.py
@@ -1,14 +1,13 @@
 from __future__ import absolute_import
+from celery import Celery
 
-    from celery import Celery
+# instantiate Celery object
+celery = Celery(include=[
+                         'proj'
+                        ])
 
-    # instantiate Celery object
-    celery = Celery(include=[
-                             'proj'
-                            ])
+# import celery config file
+celery.config_from_object('celeryconfig')
 
-    # import celery config file
-    celery.config_from_object('celeryconfig')
-
-    if __name__ == '__main__':
-        celery.start()
+if __name__ == '__main__':
+    celery.start()
