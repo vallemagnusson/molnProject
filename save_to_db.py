d6b569f7591fbdb1232697b700814a81c4bc6390
vallemagnusson <valle.magnusson@gmail.com>
2015-10-27 14:00:34 +0100

    save_to_db.py

---
+++ save_to_db.py
@@ -0,0 +1,22 @@
+import pickledb
+import sys
+import os
+import swiftclient.client
+
+def to_db(key, value):
+	dataBaseName = "pictureDatabase"
+	db = pickledb.load(dataBaseName, False)
+	dbKeys = db.getall()
+	if key not in dbKeys:
+		print 1, "Ny grej"
+		db.set(key, value)
+		db.dump()
+
+def in_db(fileName):
+	dataBaseName = "pictureDatabase"
+	db = pickledb.load(dataBaseName, False)
+	dbKeys = db.getall()
+	if fileName in dbKeys:
+		return True
+	else:
+		return False
