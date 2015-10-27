bd37e0988df2460fb61650130057338e97006f1d
vallemagnusson <valle.magnusson@gmail.com>
2015-10-27 16:26:54 +0100

    lsitan

--- proj.py
+++ proj.py
@@ -76,7 +76,7 @@ def convertFile(angle, n_nodes, n_levels, num_samples, visc, speed, T):
 	#!!!!!!!!!!!!!return (fileNameWithoutExtension+"N"+num+"v"+visc_s+"s"+speed_s+"T"+T_s+".msh", resultLists)
 	pictureName = fileNameWithoutExtension + "Num" + num + "Visc" + visc_s + "Speed" + speed_s + "T" + T_s + ".png"
 	dbName = fileNameWithoutExtension + "Num" + num + "Visc" + visc_s + "Speed" + speed_s + "T" + T_s
-	
+	print resultLists
 	plot_file(pictureName, resultLists)
 	pictureFile = open(pictureName, "r")
 	object_id = conn.put_object(bucket_name, pictureName, pictureFile)
