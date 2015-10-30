2cd11105f5bb07ad61ee6fcce960998722d0f92c
vallemagnusson <valle.magnusson@gmail.com>
2015-10-30 14:33:30 +0100

    again

--- flaskApp.py
+++ flaskApp.py
@@ -76,6 +76,7 @@ def runsh():
 				new_picture.close()
 
 	print angles
+	global result
 	if len(angles) != 0:
 		print "Nu skickas allt ivag :)"
 		time_to_calculate = time.time()
@@ -83,7 +84,7 @@ def runsh():
 		print "Time for response: " + str(time.time() - time_to_calculate)
 
 		time_to_calculate_2 = time.time()
-		global result = response.apply_async()
+		result = response.apply_async()
 		print "Time for result var: " + str(time.time() - time_to_calculate_2)
 		result.get()
 		print "Time to calculate all: " + str(time.time() - time_to_calculate)
