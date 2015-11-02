d78c76a783a6e8d81e93f44f8d4e1c8fe17b3e75
vallemagnusson <valle.magnusson@gmail.com>
2015-11-02 23:54:57 +0100

    a

--- proj.py
+++ proj.py
@@ -75,7 +75,7 @@ def convertFile(angle, n_nodes, n_levels, num_samples, visc, speed, T):
 	visc_s = str(visc)
 	speed_s = str(speed)
 	T_s = str(T)
-	subprocess.check_call(["sudo","."+ fileLocation +"/airfoil", num, visc_s, speed_s, T_s, xmlFileName], stdout=FNULL, stderr=subprocess.STDOUT)
+	subprocess.check_call(["sudo","./airfoil", num, visc_s, speed_s, T_s, xmlFileName], cwd=fileNameWithoutExtension+"/", stdout=FNULL, stderr=subprocess.STDOUT)
 	#############################################################
 	# Extracting information from frag_ligt.m 
 	#############################################################	
